9755c446a6fa77009515e8b8f7221e091bbb1270
skrillex007 <evil.dalas@yandex.ru>
2025-01-31 11:20:38 +0300

    FirstCommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
     Student student=new Student();
+    int[] phoneNumbers={1,2,3,4,5,6,7,8,9,0};
+    String formattedPhoneNumber=Kata.createPhoneNumber(phoneNumbers);
+        System.out.println(formattedPhoneNumber);
     }
 }
