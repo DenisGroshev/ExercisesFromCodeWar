107902cd8575c779c14a1479071ee4bc554b5ca9
skrillex007 <evil.dalas@yandex.ru>
2025-02-01 11:38:49 +0300

    Little changes for make a branch

--- CalcOutput/src/main/java/Printer.java
+++ CalcOutput/src/main/java/Printer.java
@@ -2,7 +2,7 @@ import evguru.calc.Calculator;
 
 public class Printer {
     public static void main(String[] args) {
-        int result = Calculator.add(5,7);
+        int result = Calculator.add(5,8);
         System.out.println(result);
     }
 }
